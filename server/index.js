0c2e5d2e0d8dd137d6575f62875800fed0b1580e
wais-1 <brianeditzzx1@gmail.com>
2026-06-04 11:45:23 +0300

    fix cors for gh-pages

--- server/index.js
+++ server/index.js
@@ -4,12 +4,14 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ 
+  origin: ['http://localhost:5173', 'https://wais-1.github.io'] 
+}));
 app.use(express.json());
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth',  require('./routes/auth'));
-app.use('/api/dinoCard', require('./routes/dinoCard')); 
+app.use('/api/dinoCard', require('./routes/dinoCard'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
