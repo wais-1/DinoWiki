cf3d5468782939f14ac4a7bd31f1222a001b6560
wais-1 <brianeditzzx1@gmail.com>
2026-06-06 14:59:26 +0300

    correct adding pics

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 
 const app = express();
 
+const path = require('path')
+
 app.use(cors());
 app.use(express.json());
 
@@ -11,7 +13,7 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth',  require('./routes/auth'));
 app.use('/api/dinoCard', require('./routes/dinoCard'));
 app.use('/api/dinoPage', require('./routes/dinoPage'));
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
